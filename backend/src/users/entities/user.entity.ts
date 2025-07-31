12ddeb624b43563c06c971d4c4fd0461b13bb419
Kevin-ORojas <kevinroj326@gmail.com>
2025-07-31 15:38:02 -0500

    user controller, user service

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class User {
+  @Column({ primary: true, generated: true })
+  id: number;
+
+  @Column({ length: 500 })
+  name: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: number;
+}
