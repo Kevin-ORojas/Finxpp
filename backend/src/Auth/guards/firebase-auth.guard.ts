ec2c1f03d17bb591fa21189bfc47ff63a0d6ab37
Kevin-ORojas <kevinroj326@gmail.com>
2025-07-31 09:34:47 -0500

    guardas auth

--- backend/src/Auth/guards/firebase-auth.guard.ts
+++ backend/src/Auth/guards/firebase-auth.guard.ts
@@ -1,14 +1,26 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { FireBaseService } from '../../firebase/firebase.service';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
+import { auth } from 'firebase-admin';
 
 @Injectable()
-
 export class FirebaseAuthGuard implements CanActivate {
-    constructor(private firebaseService: FireBaseService){}
-    async canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const req = context.switchToHttp().getRequest()
+  constructor(private firebaseService: FireBaseService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader || !authHeader.startWith('Bearer')) {
+      throw new UnauthorizedException('Token no proporcionado');
     }
 
-
+    const token = authHeader.split(' ')[1];
+  }
 }
