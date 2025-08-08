96bbb4e95de92c877904d6f1bceb1a9861994501
Kevin-ORojas <kevinroj326@gmail.com>
2025-08-07 21:06:36 -0500

    dash

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,8 +1,28 @@
+import { Box, Grid } from "@mui/material";
+
 function Dashboard() {
+  const datos = "datos";
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <Box>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Box sx={{ minWidth: 200, height: 200, border: "2px solid" }}>
+          {datos}
+        </Box>
+        <Box sx={{ minWidth: 200, height: 200, border: "2px solid" }}>
+          {datos}
+        </Box>
+        
+      </Grid>
+      
+    </Box>
   );
 }
 
