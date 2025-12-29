bfd8a81c6def6f979098031cb7a31a1bc5c90004
Kevin-ORojas <kevinroj326@gmail.com>
2025-12-28 21:43:44 -0500

    Add InvoiceLine model and update Invoice model to include Lines property

--- backend/Models/InvoiceLine.cs
+++ backend/Models/InvoiceLine.cs
@@ -1,4 +1,17 @@
+namespace backend.Models;
+
 public class InvoiceLine
 {
+    public int InvoiceLineID { get; set; }
+
+    public int InvoiceId { get; set; }
+    public Invoice Invoice { get; set; } = null!;
+
+    public string Description { get; set; } = string.Empty;
+
+    public int Quantity { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
 
 }
