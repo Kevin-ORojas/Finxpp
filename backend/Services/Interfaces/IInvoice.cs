a0751483bed036e9ba937d49c699deea12d64af0
Kevin-ORojas <kevinroj326@gmail.com>
2026-01-01 12:31:34 -0500

    feature new branch

---
+++ backend/Services/Interfaces/IInvoice.cs
@@ -0,0 +1,13 @@
+namespace backend.Services.Interfaces;
+
+using backend.Data;
+using backend.DTOs;
+using backend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+public class InvoiceService : IInvoiceService
+{
+    private readonly AppDbContextontext _context;
+
+
+}
